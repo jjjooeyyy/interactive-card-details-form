d28778ee11bcdac017fec0c5b719a89c1572c86e
JOEY <puiying526@gmail.com>
2023-04-27 14:23:23 +0100

    work with JS validation

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// Info show on the card img 
+const holderName = document.getElementById('cardName');
+const cardNum = document.getElementById('cardNum');
+const expDate = document.getElementById('cardDate');
+const cvc = document.getElementById('cardCVC');
+
+// Info from user's input 
+const inputName = document.getElementById('inputName');
+const inputCardNum = document.getElementById('inputCardNum');
+const inputMM = document.getElementById('inputMM');
+const inputYY = document.getElementById('inputYY');
+const inputCVC = document.getElementById('inputCVC');
+
+// Submit form button, error Msg
+const btn = document.getElementById('submit');
+const errorMsg = document.querySelector('.errorMsg');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
